91d48e9967fb36ec99ffbbf8ad12201fb1653403
Stephen Turner <vustephen@gmail.com>
2017-11-28 12:06:26 -0500

    more work on flu data

--- _dev/flu-caret.R
+++ _dev/flu-caret.R
@@ -20,20 +20,23 @@
 # show data cleaning / feature extraction, then provide analysis-ready data to use?
 
 
-# packages needed by caret ----
-library(caret)
-library(randomForest)
-library(gbm)
-# others called by caret in model comparison section below
-
-
 # init ----
 
 library(tidyverse)
 library(here)
-library(caret)
 theme_set(theme_bw())
 setwd(here("_dev"))
+
+
+# packages needed by caret ----
+library(caret)
+library(randomForest)
+library(gbm)
+library(glmnet)
+library(kknn)
+# others called by caret in model comparison section below
+
+
 
 
 # create data ----
@@ -53,32 +56,41 @@ outbreaks::fluH7N9_china_2013 %>%
   # make this more explicitly a string/factor
   mutate(case_id=paste0("case_", case_id)) %>%
   as_tibble() %>%
-  write_csv(here("data", "fluH7N9_china_2013.csv"))
+  write_csv(here("data", "h7n9.csv"))
 
-flu <- read_csv(here("data", "fluH7N9_china_2013.csv"))
+flu <- read_csv(here("data", "h7n9.csv"))
 flu
 
 
 # EDA ----
-flu <- read_csv(here("data", "fluH7N9_china_2013.csv"))
+
+# Read in data
+flu <- read_csv(here("data", "h7n9.csv"))
 flu
 
-ggplot(flu, aes(age)) +
-  geom_density(aes(fill=outcome), alpha=1/4) +
-  geom_rug(aes(color=outcome))
+# Feel free to follow along but don't worry if not grokking code here, not doing
+# any data transformations that needed later.
 
+# Look at the distribution of death, recovery, and missing, by age
+ggplot(flu, aes(age)) +
+  geom_density(aes(fill=outcome), alpha=1/3)
+
+# Look at distribution by gender across provinces
 ggplot(flu, aes(gender)) +
   geom_bar(aes(fill=outcome), position="dodge") +
   facet_wrap(~province)
 
+# Show the distribution of age by province, colored by outcome. This shows that
+# there's a higher rate of death in older individuals but this is only observed
+# in Jiangsu and Zhejiang provinces.
+ggplot(flu, aes(province, age)) + geom_boxplot(aes(fill=outcome))
+
+# Gather the data where we have the different date variables as a column
 flugather <- flu %>%
-  gather(group, date, starts_with("date_"))
+  gather(key, date, starts_with("date_"))
 flugather
-
-ggplot(flugather, aes(date, age)) +
-  geom_point(aes(color=outcome, shape=gender)) +
-  facet_grid(group~province)
-
+# Show how many days passed between onset, hospitalization, and outcome, for each case.
+# Lots of missing data points, difficult to draw a conclusion.
 ggplot(flugather, aes(date, y=age, color=outcome)) +
   geom_point() +
   geom_path(aes(group=case_id)) +
@@ -89,8 +101,8 @@ ggplot(flugather, aes(date, y=age, color=outcome)) +
 median(flu$date_outcome, na.rm=TRUE)
 
 fludata <- flu %>%
-  # make female yes/no if gender is F, then remove gender. necessary?
-  mutate(female = gender=="f") %>% select(-gender) %>%
+  # make male yes/no if gender is F, then remove gender.
+  mutate(male = gender=="m") %>% select(-gender) %>%
   # Was someone ever hospitalized?
   mutate(hospital = !is.na(date_hospitalization)) %>%
   mutate(days_to_hospital = as.numeric(date_hospitalization - date_onset)) %>%
@@ -104,11 +116,20 @@ fludata
 # is this dummy-variable creation necessary or will caret just do it for you? test both ways.
 model.matrix(~0+province, data=fludata)
 
+# bind the columns with the original data
+cbind(fludata, model.matrix(~0+province, data=fludata))
+
+# turn it into a tibble, remove the province variable
 fludata <- cbind(fludata, model.matrix(~0+province, data=fludata)) %>%
   as_tibble() %>%
-  select(-province) %>%
-  set_names(~gsub("province", "", .))
+  select(-province)
 
+fludata
+
+# Remove "province" from the names
+names(fludata)
+gsub("province", "", names(fludata))
+names(fludata) <- gsub("province", "", names(fludata))
 fludata
 
 
@@ -117,11 +138,14 @@ fludata
 
 library(mice)
 
+# "outcome" is what we want to try to predict, but we need to impute missing values in other variables.
+# take flu data, remove case id and outcome variable, run mice to impute
 fluimp <- fludata %>%
   select(-1, -2) %>%
   mice(print=FALSE) %>%
   complete()
 
+# put the data back together
 fluimp <- fludata %>%
   select(1,2) %>%
   cbind(fluimp) %>%
@@ -129,7 +153,7 @@ fluimp <- fludata %>%
 
 fluimp
 
-fluimp %>% write_csv(here("data", "fluH7N9_china_2013_analysisready.csv"))
+fluimp %>% write_csv(here("data", "h7n9_analysisready.csv"))
 
 
 # test/train/validation ----
@@ -137,15 +161,17 @@ fluimp %>% write_csv(here("data", "fluH7N9_china_2013_analysisready.csv"))
 library(tidyverse)
 library(here)
 library(caret)
-fluimp <- read_csv(here("data", "fluH7N9_china_2013_analysisready.csv"))
+fluimp <- read_csv(here("data", "h7n9_analysisready.csv"))
 
 
 fluimp
 
+# these are samples with unknown data we'll use later to predict
 unknown <- fluimp %>%
   filter(is.na(outcome))
 unknown
 
+# samples with known outcomes
 known <- fluimp %>%
   filter(!is.na(outcome)) %>%
   select(-case_id)
@@ -167,35 +193,72 @@ known
 # set.seed(27)
 # modrf <- train(outcome~., data=known, method="rf", preProcess=NULL, trControl=control)
 
-set.seed(27)
+# random forest
+set.seed(42)
 modrf <- train(outcome~., data=known, method="rf")
 modrf
 varImp(modrf, scale=TRUE)
 varImp(modrf, scale=TRUE) %>% plot()
 
-set.seed(28)
+# gradient boosting
+set.seed(43)
 modgbm <- train(outcome~., data=known, method="gbm", verbose=FALSE)
 modgbm
-varImp(modrf, scale=TRUE)
-varImp(modrf, scale=TRUE) %>% plot()
+varImp(modgbm, scale=TRUE)
+varImp(modgbm, scale=TRUE) %>% plot()
 
 # compare models
 modsum <- resamples(list(gbm=modgbm, rf=modrf))
 summary(modsum)
 
+# LASSO / Elastic-Net Regularized Generalized Linear Models
+set.seed(44)
+modlasso <- train(outcome~., data=known, method="glmnet")
+modlasso
+
+# k-nearest neighbors
+set.seed(45)
+modknn <- train(outcome~., data=known, method="kknn")
+modknn
+
+
+# compare models
+modsum <- resamples(list(gbm=modgbm, rf=modrf, lasso=modlasso, knn=modknn))
+summary(modsum)
+bwplot(modsum)
+
 
 # predict the unknowns
 predict(modrf, newdata=unknown)
-predict(modrf, newdata=unknown, type="prob")
 # get them back in the data
 unknown
 unknown %>%
   mutate(outcome=predict(modrf, newdata=unknown))
 
+# Prediction probabilities
+predict(modrf, newdata=unknown, type="prob") %>% head()
+
+# # sum up prediction probabilities
+# predict(modrf, newdata=unknown, type="prob")
+# data.frame(rf    = predict(modrf,    newdata=unknown, type="prob"),
+#            gbm   = predict(modgbm,   newdata=unknown, type="prob"),
+#            knn   = predict(modknn,   newdata=unknown, type="prob"),
+#            lasso = predict(modlasso, newdata=unknown, type="prob")) %>%
+#   as_tibble() %>%
+#   mutate(sumdeath = rf.Death + gbm.Death + knn.Death + lasso.Death,
+#          sumrecover = rf.Recover + gbm.Recover + knn.Recover + lasso.Recover,
+#          log2ratio=log2(sumdeath/sumrecover)) %>%
+#   mutate(outcome=case_when(
+#     log2ratio > 1 ~ "Death",
+#     log2ratio < -1 ~ "Recover",
+#     TRUE ~ "UNCERTAIN"
+#   )) %>% View
+
+
 # check that distributions are somewhat similar.
 fluimp %>%
   gather(key, value, age, days_to_hospital, days_to_outcome) %>%
-  ggplot(aes(outcome, value, fill=factor(female))) + geom_boxplot() + facet_wrap(~key, scale="free_y")
+  ggplot(aes(outcome, value, fill=factor(male))) + geom_boxplot() + facet_wrap(~key, scale="free_y")
 
 
 
@@ -210,30 +273,37 @@ geographic_spread(2015)
 ?hospitalizations
 ?ilinet
 
-flu <- ilinet()
-flu
-flu <- flu %>% select(week_start, ilitotal, total_patients)
-flu
+ili <- ilinet()
+ili
+ili <- ili %>% select(week_start, ilitotal, total_patients)
+ili
 
 pi <- pi_mortality(coverage_area = "national")
 pi
 pi <- pi %>% transmute(week_start=wk_start+1, fludeaths=number_influenza, pneumoniadeaths=number_pneumonia, all_deaths)
 pi
 
-fludata <- left_join(flu, pi, by="week_start") %>% filter(!is.na(week_start))
-fludata <- fludata %>% filter(year(week_start)>=2003)
+ilidata <- ili %>%
+  left_join(pi, by="week_start") %>%
+  filter(!is.na(week_start)) %>%
+  filter(week_start>="2003-01-01" & week_start<="2017-10-31")
+
+ilidata %>% write_csv(here::here("data", "ilinet.csv"))
 
 
-ggplot(fludata, aes(week_start, ilitotal)) + geom_line()
-ggplot(fludata, aes(week_start, fludeaths)) + geom_line()
+library(tidyverse)
+ilidata
+ilidata %>% tail
+ggplot(ilidata, aes(week_start, ilitotal)) + geom_line()
+ggplot(ilidata, aes(week_start, fludeaths)) + geom_line()
 
 
-fludata
+ilidata
 
 library(prophet)
-fludata
+ilidata
 ?prophet
-m <- fludata %>%
+m <- ilidata %>%
   select(ds=week_start, y=ilitotal) %>%
   prophet
 future <- make_future_dataframe(m, periods=365*5)
@@ -243,11 +313,12 @@ plot(m, forecast)
 prophet_plot_components(m, forecast)
 prophet:::plot.prophet
 
-m <- fludata %>%
+m <- ilidata %>%
   filter(!is.na(fludeaths)) %>%
   select(ds=week_start, y=fludeaths) %>%
   prophet
 future <- make_future_dataframe(m, periods=365*5)
 forecast <- predict(m, future)
 tail(forecast)
+plot(m, forecast)
 prophet_plot_components(m, forecast)
